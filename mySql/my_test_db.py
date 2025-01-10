95edc520bf30d19223e134f7624cd436791ad738
denis <Den_1994_2015@mail.ru>
2025-01-10 09:41:39 +0300

    my forth commit

--- mySql/my_test_db.py
+++ mySql/my_test_db.py
@@ -10,33 +10,58 @@ db = mysql.connector.connect(host="localhost",
 cursor = db.cursor()
 
 # создал таблицу worckers
-#query = """CREATE TABLE worckers(
-#            id INTEGER,
-#            name VARCHAR(50),
-#            first_name VARCHAR(50),
-#            address VARCHAR(50),
-#            phone VARCHAR(11)
-#            );
-#        """
+query = """CREATE TABLE worckers(
+            id INTEGER,
+            name VARCHAR(50),
+            first_name VARCHAR(50),
+            address VARCHAR(50),
+            phone VARCHAR(11)
+            );
+        """
+
+cursor.execute(query)
+db.commit()
 
 
 # добавил работника
-#query = """INSERT INTO worckers VALUES (1, 'Denis', 'Popov', 'lenina 45', '89203415725')"""
+query = """INSERT INTO worckers VALUES (1, 'Denis', 'Popov', 'lenina 45', '89203415725')"""
 
-# добавил второго
-#query = """INSERT INTO worckers VALUES (2, 'Vasay', 'Sidorov', 'kuchigina 43', '89501324782')"""
-
-
-
-#query = """SELECT * FROM worckers;"""
 cursor.execute(query)
 db.commit()
+
+# добавил второго
+query = """INSERT INTO worckers VALUES (2, 'Vasay', 'Sidorov', 'kuchigina 43', '89501324782')"""
+
+cursor.execute(query)
+db.commit()
+
+# добавил столбец profession
+query = """ALTER TABLE worckers ADD COLUMN profession VARCHAR(50);"""
+
+cursor.execute(query)
+db.commit()
+
+# установил специальность для первого работника
+query = """UPDATE worckers  SET 
+           profession = 'backend developer' 
+           WHERE id = 1;"""
+
+cursor.execute(query)
+db.commit()
+
+# добавил специальность для второго работника
+query = """UPDATE worckers SET 
+            profession = 'frontent developer' 
+            WHERE id = 2;"""
+
+cursor.execute(query)
+db.commit()
+
+query = """SELECT * FROM worckers;"""
+cursor.execute(query)
+
 
 
 
 for row in cursor:
     print(row)
-
-
-
-
