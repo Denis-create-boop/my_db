f235ea267007d7bdee002cd7da2f041a4a18e1d9
denis <Den_1994_2015@mail.ru>
2025-01-27 20:30:45 +0300

    created two classes

--- mySql/my_test_db_professions.py
+++ mySql/my_test_db_professions.py
@@ -1,40 +1,75 @@
 import mysql.connector
-from main_my_test_db import SECRET_KEY
+import sys
+sys.path.append("C:\\Users\\Den_1\\Desktop\\packeges")
+
+from main import SECRET_KEY
+
+
+db = mysql.connector.connect(
+    host="localhost", database="my_test_db", user="root", password=SECRET_KEY
+)
 # crete database my_test_db
 
 
-db = mysql.connector.connect(host="localhost", 
-                               database="my_test_db", 
-                               user="root", 
-                               password=SECRET_KEY)
+class Professions:
+
+    def __init__(self):
+        self.cursor = db.cursor()
 
 
-cursor = db.cursor()
+    def create(self):
+        query = """CREATE TABLE IF NOT EXISTS professions 
+        (id INTEGER, profession VARCHAR(50), amount VARCHAR(50))"""
+        self.cursor.execute(query)
+        db.commit()
+
+    
+    def get_id(self):
+        query = """SELECT MAX(id) FROM professions"""
+        self.cursor.execute(query)
+        for row in self.cursor:
+            if row[0] is None:
+                return 1
+            else:
+                return int(row[0]) + 1
+        
+
+    def get_amount(self, prof):
+        query = """SELECT amount FROM professions WHERE profession = %s"""
+        self.cursor.execute(query, (prof, ))
+        for row in self.cursor:
+            return row
 
 
-# создал таблицу professions с полями id - порядковый номер, profession - название провессии, 
-# floor - номер этажа на котором находиться этот отдел
-query = """CREATE TABLE professions(
-            id INTEGER,
-            profession VARCHAR(50),
-            floor INTEGER);      
-        """
-
-cursor.execute(query)
-db.commit()
+    def show_all(self):
+        query = """SELECT * FROM professions"""
+        self.cursor.execute(query)
+        return self.cursor
 
 
-# добавил профессии в таблицу
-query = """INSERT INTO professions (id, profession, floor)
-                VALUES (1, 'backend developer', 2),
-                (2, 'frontend developer', 3),
-                (3, 'QA manager', 4),
-                (4, 'software tester', 4);
-        """
+    def show_profession(self):
+        query = """SELECT profession FROM professions"""
+        ls_prof = []
+        cursor.execute(query)
+        for row in cursor:
+            ls_prof.append(row[0])
+        return ls_prof
+    
 
+    def write(self, prof):
+        self.create()
+        id = self.get_id()
+        amount = self.get_amount(prof)
+        if amount is None:
+            amount = 0
+        else:
+            amount = int(amount[0]) + 1
+        ls_prof = self.show_profession()
+        
+        if prof not in ls_prof or ls_prof is None:
+            query = """INSERT INTO professions (id, profession, amount) VALUES(%s, %s, %s)"""
+            ls = (id, prof, amount,)
+            self.cursor.execute(query, ls)
+            db.commit()
 
-
-cursor.execute(query)
-db.commit()
-
-
+            
--- mySql/my_test_db_worckers.py
+++ mySql/my_test_db_worckers.py
@@ -1,62 +1,31 @@
-import mysql.connector
-from main_my_test_db import SECRET_KEY
+from my_test_db_professions import db
 # crete database my_test_db
 
 
+class Worckers:
+    def __init__(self):
+        self.cursor = db.cursor()
 
-db = mysql.connector.connect(host="localhost", 
-                               database="my_test_db", user="root", 
-                               password=SECRET_KEY)
-
-
-cursor = db.cursor()
-
-# создал таблицу worckers
-query = """CREATE TABLE worckers(
+    def create(self):
+        # создал таблицу worckers
+        query = """CREATE TABLE IF NOT EXISTS worckers(
             id INTEGER,
             name VARCHAR(50),
             first_name VARCHAR(50),
             address VARCHAR(50),
-            phone VARCHAR(11)
+            phone VARCHAR(11),
+            profession VARCHAR(50)
             );
         """
+        self.cursor.execute(query)
+        db.commit()
 
-cursor.execute(query)
-db.commit()
 
-
-# добавил работника
-query = """INSERT INTO worckers VALUES (1, 'Denis', 'Popov', 'lenina 45', '89203415725')"""
-
-cursor.execute(query)
-db.commit()
-
-# добавил второго
-query = """INSERT INTO worckers VALUES (2, 'Vasay', 'Sidorov', 'kuchigina 43', '89501324782')"""
-
-cursor.execute(query)
-db.commit()
-
-# добавил столбец profession
-query = """ALTER TABLE worckers ADD COLUMN profession VARCHAR(50);"""
-
-cursor.execute(query)
-db.commit()
-
-# установил специальность для первого работника
-query = """UPDATE worckers  SET 
-           profession = 'backend developer' 
-           WHERE id = 1;"""
-
-cursor.execute(query)
-db.commit()
-
-# добавил специальность для второго работника
-query = """UPDATE worckers SET 
-            profession = 'frontend developer' 
-            WHERE id = 2;"""
-
-cursor.execute(query)
-db.commit()
-
+    def write(self, name, first_name, address, phone, profession):
+        self.create()
+        query = """INSERT INTO worckers (id, name, first_name, address, phone, profession) 
+        VALUES (%s, %s, %s, %s, %s, %s)"""
+        ls_write = (name, first_name, address, phone, profession)
+        self.cursor.execute(query, ls_write)
+        db.commit()
 
