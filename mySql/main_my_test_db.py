ae14c55f16422e42b6e012e6b6edb91a8b38fb19
denis <Den_1994_2015@mail.ru>
2025-02-01 15:24:21 +0300

    added class Profession for worck with database professions

--- mySql/main_my_test_db.py
+++ mySql/main_my_test_db.py
@@ -1,72 +1,12 @@
-from my_test_db_professions import Professions
-from my_test_db_worckers import Worckers
 
-obj_prof = Professions()
+from my_test_db_worckers import Worckers
+from professions import Profession
+
 obj_worck = Worckers()
 
 
-def add_prof():
-    print("Введите профессию")
-    profession = input()
-    obj_prof.write(profession)
-    print("Профессия успешно добавлена")
-    input()
-    return
+obj_prof = Profession()
 
-
-def del_prof() -> None:
-    print("Введите профессию которую хотите удалить")
-    profession = input()
-    print(obj_prof.del_prof(profession))
-    input()
-    return
-
-
-def change_prof_name():
-    print("Введите id профессии которую хотите изменить")
-    id = int(input())
-    print("Введите новое название")
-    profession = input()
-    print(obj_prof.change_name(id, profession))
-    input()
-    return
-
-
-def prof():
-    while True:
-        doit = {
-            "1": add_prof,
-            "3": del_prof,
-            "4": change_prof_name,
-            "добавить новую профессию": add_prof,
-            "удалить профессиию": del_prof,
-            "изменить название профессии": change_prof_name,
-        }
-        print(f"""Вы выбрали работу с профессиями, выберите команду:
-                  1 - Добавить новую профессию
-                  2 - Посмотреть все профессии
-                  3 - Удалить профессиию
-                  4 - Изменить название профессии
-                  5 - Посмотреть все данные таблицы
-                  6 - Назат
-                  7 - Выход""")
-        answer = input()
-        if int(answer) == 6 or answer.lower() == "назат":
-            return
-        elif int(answer) == 7 or answer.lower() == "выход":
-            return 7
-        elif int(answer) == 5 or answer.lower() == "посмотреть все данные таблицы":
-            for row in obj_prof.show_all():
-                print(row)
-            input()
-            return
-        elif int(answer) == 2 or answer.lower() == "посмотреть все профессии":
-            for row in obj_prof.show_profession():
-                print(row)
-            input()
-            return
-        else:
-            doit[answer.lower()]()
 
 def worcker():
     pass
@@ -81,7 +21,7 @@ def main():
         
         answer = input()
         if int(answer) == 1 or answer.lower() == "работа с профессиями":
-            step = prof()
+            step = obj_prof.prof()
             if step == 7:
                 break
 
---
+++ mySql/professions.py
@@ -0,0 +1,74 @@
+from my_test_db_professions import Professions
+
+
+class Profession:
+
+    def __init__(self):
+        self.obj_prof = Professions()
+
+    def add_prof(self):
+        print("Введите профессию")
+        profession = input()
+        self.obj_prof.write(profession)
+        print("Профессия успешно добавлена")
+        input()
+        return
+
+    def del_prof(self) -> None:
+        print("Введите профессию которую хотите удалить")
+        profession = input()
+        print(self.obj_prof.del_prof(profession))
+        input()
+        return
+
+    def change_prof_name(self):
+        print("Введите id профессии которую хотите изменить")
+        id = int(input())
+        print("Введите новое название")
+        profession = input()
+        print(self.obj_prof.change_name(id, profession))
+        input()
+        return
+
+    # function for worcking with professions
+    def prof(self):
+        while True:
+            doit = {
+                "1": self.add_prof,
+                "3": self.del_prof,
+                "4": self.change_prof_name,
+                "добавить новую профессию": self.add_prof,
+                "удалить профессиию": self.del_prof,
+                "изменить название профессии": self.change_prof_name,
+            }
+            print(
+                f"""Вы выбрали работу с профессиями, выберите команду:
+                      1 - Добавить новую профессию
+                      2 - Посмотреть все профессии
+                      3 - Удалить профессиию
+                      4 - Изменить название профессии
+                      5 - Посмотреть все данные таблицы
+                      6 - Назат
+                      7 - Выход"""
+            )
+            answer = input()
+            if int(answer) == 6 or answer.lower() == "назат":
+                return
+            elif int(answer) == 7 or answer.lower() == "выход":
+                return 7
+            elif int(answer) == 5 or answer.lower() == "посмотреть все данные таблицы":
+                for row in self.obj_prof.show_all():
+                    print(
+                        f"id: {row[0]}, профессия:\n{row[1]},\nколичество сотрудников: {row[2]}"
+                    )
+                    print("--------------------------")
+                    print()
+                input()
+                return
+            elif int(answer) == 2 or answer.lower() == "посмотреть все профессии":
+                for row in self.obj_prof.show_profession():
+                    print(row)
+                input()
+                return
+            else:
+                doit[answer.lower()]()
